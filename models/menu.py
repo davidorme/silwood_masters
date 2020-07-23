500ee83989ad53fe86d34f76e425af9f67be748c
dorme <d.orme@imperial.ac.uk>
2020-07-23 19:33:22 +0100

    broken link

--- models/menu.py
+++ models/menu.py
@@ -5,7 +5,7 @@
 response.menu = [
     (T('Home'), False, URL('default', 'index'), []),
     (T('Module Grid'), False, URL('default', 'module_grid'), []),
-    (T('Room Grid'), False, URL('default', 'module_grid'), []),
+    (T('Room Grid'), False, URL('default', 'room_grid'), []),
     (T('Tables'), False, None, [
         (T('College Dates'), False, URL('default', 'college_dates'), []),
         (T('Locations'), False, URL('default', 'locations'), []),
