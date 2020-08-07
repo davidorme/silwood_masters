9237c2f265f4c881575c7943b3a96c86deeefe4e
dorme <d.orme@imperial.ac.uk>
2020-08-07 13:06:49 +0100

    Merging mr and tt applications: marking report menu links

--- models/marking_reports/menu.py
+++ models/marking_reports/menu.py
@@ -1,6 +1,5 @@
 marking_reports_menu = [
-    (T('Home'), False, URL('default', 'index'), []),
-    (T('Project Proposals'), False, URL('default','project_proposals'), []),
+    (T('Project Proposals'), False, URL('marking_reports','project_proposals'), []),
     (T('Dissertation Reports'), False, None, [
         (T('2019-20'), False, 'https://drive.google.com/drive/folders/12vkP0t2D8WXr9tsiOxH5DL-jX6khIypY', []),
         ]),
@@ -13,17 +12,17 @@ marking_reports_menu = [
 if auth.is_logged_in():
     marking_reports_menu.append((T('Admin tools'), False, None, [
                                   (T('Create Marking Assignment'), False, 
-                                   URL('default', 'new_assignment'), []),
+                                   URL('marking_reports', 'new_assignment'), []),
                                   (T('Load Marking Assignments'), False, 
-                                   URL('default', 'load_assignments'), []),
+                                   URL('marking_reports', 'load_assignments'), []),
                                   (T('View Marking Assignments'), False, 
-                                   URL('default', 'assignments'), []),
+                                   URL('marking_reports', 'assignments'), []),
                                   (T('View Project Markers'), False, 
-                                   URL('default', 'markers'), []),
+                                   URL('marking_reports', 'markers'), []),
                                   (T('View Email Log'), False, 
-                                   URL('default', 'email_log'), []),
+                                   URL('marking_reports', 'email_log'), []),
                                   (T('Database (requires site password)'), False, 
                                    URL(app, 'appadmin', 'index'))
-                                  ])
+                                  ]))
 
-response.menu.append(marking_reports_menu)
+response.menu.extend(marking_reports_menu)
