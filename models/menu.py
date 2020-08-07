2237cde234c24770440dc810b505d6d244dfe8f0
dorme <d.orme@imperial.ac.uk>
2020-08-07 12:21:25 +0100

    Merging mr and tt applications: menu logo

--- models/menu.py
+++ models/menu.py
@@ -4,6 +4,10 @@
 # This minimal constant menu is extended in the controller model folders
 # so that it changes by the controller context
 
+response.logo = IMG(_src=URL('static','images/imperial_white.png'), _height='70px')
+response.title = request.application.replace('_',' ').title()
+response.subtitle = ''
+
 response.menu = [
     (T('Home'), False, URL('default', 'index'), []),
     (T('Marking'), False, URL('marking', 'index'), []),
