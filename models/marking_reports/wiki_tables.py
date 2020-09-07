65ce8f8ea1cc82b7c94a0f16a11b8fb991087842
dorme <d.orme@imperial.ac.uk>
2020-09-07 11:19:50 +0100

    turned on signing on wiki tables

--- models/marking_reports/wiki_tables.py
+++ models/marking_reports/wiki_tables.py
@@ -1,3 +1,7 @@
+## Turn on signed tables
+db._common_fields.append(auth.signature)
+
+
 db.define_table('wikimedia',
                 Field('slug','string', unique=True, requires=IS_SLUG(check=True)),
                 Field('mediafile','upload'))
